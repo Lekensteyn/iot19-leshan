3e410309dff7e1e1b71d4ef3b298188e1186b564
Peter Wu <peter@lekensteyn.nl>
2017-01-07 21:00:44 +0100

    WIP for loading our custom Light Profile object spec

--- client/src/main/java/group19/Client.java
+++ client/src/main/java/group19/Client.java
@@ -2,14 +2,23 @@ package group19;
 
 import static org.eclipse.leshan.LwM2mId.FIRMWARE;
 
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.leshan.client.californium.LeshanClient;
 import org.eclipse.leshan.client.californium.LeshanClientBuilder;
 import org.eclipse.leshan.client.resource.LwM2mObjectEnabler;
 import org.eclipse.leshan.client.resource.ObjectsInitializer;
+import org.eclipse.leshan.core.model.LwM2mModel;
+import org.eclipse.leshan.core.model.ObjectLoader;
+import org.eclipse.leshan.core.model.ObjectModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Client {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Client.class);
 
 	public final static int LIGHT_PROFILE_ID = 10250;
 	public final static int SENSOR_PROFILE_ID = 10350;
@@ -18,10 +27,24 @@ public class Client {
 		createClient("light");
 	}
 
+	private static void loadSpec(List<ObjectModel> models, String resourceName) {
+		InputStream input = ObjectLoader.class.getResourceAsStream(resourceName);
+		if (input != null) {
+			models.addAll(ObjectLoader.loadJsonStream(input));
+		} else {
+			LOG.error("Unable to load resource: " + resourceName);
+		}
+	}
+
 	public static void createClient(String endpoint) {
+		// Load LWM2M specifications (include default OMA classes for for Firmware profile)
+		List<ObjectModel> models = new ArrayList<>();
+		loadSpec(models, "/resources/oma-objects-spec.json");
+		loadSpec(models, "/resources/light-object-spec.json");
+		LwM2mModel model = new LwM2mModel(models);
+		ObjectsInitializer initializer = new ObjectsInitializer(model);
 
 		// register Objects by their ID
-		ObjectsInitializer initializer = new ObjectsInitializer();
 		initializer.setClassForObject(LIGHT_PROFILE_ID, LightDevice.class);
 
 		// creates the Object Instances
