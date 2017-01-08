2a38127fbf6d7a64d78dde47cd17d5c43917c8d8
Peter Wu <peter@lekensteyn.nl>
2017-01-08 10:52:27 +0100

    Register some mandatory objects
    Now the client can actually run.

--- client/src/main/java/group19/Client.java
+++ client/src/main/java/group19/Client.java
@@ -1,6 +1,6 @@
 package group19;
 
-import static org.eclipse.leshan.LwM2mId.FIRMWARE;
+import static org.eclipse.leshan.client.object.Security.noSec;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -8,11 +8,15 @@ import java.util.List;
 
 import org.eclipse.leshan.client.californium.LeshanClient;
 import org.eclipse.leshan.client.californium.LeshanClientBuilder;
+import org.eclipse.leshan.client.object.Device;
+import org.eclipse.leshan.client.object.Server;
 import org.eclipse.leshan.client.resource.LwM2mObjectEnabler;
 import org.eclipse.leshan.client.resource.ObjectsInitializer;
 import org.eclipse.leshan.core.model.LwM2mModel;
 import org.eclipse.leshan.core.model.ObjectLoader;
 import org.eclipse.leshan.core.model.ObjectModel;
+import org.eclipse.leshan.core.request.BindingMode;
+import org.eclipse.leshan.LwM2mId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +28,8 @@ public class Client {
 	public final static int SENSOR_PROFILE_ID = 10350;
 
 	public static void main(String[] args) {
-		createClient("light");
+		String serverURI = "coap://localhost:5683";
+		createClient("light", serverURI);
 	}
 
 	private static void loadSpec(List<ObjectModel> models, String resourceName) {
@@ -36,19 +41,27 @@ public class Client {
 		}
 	}
 
-	public static void createClient(String endpoint) {
-		// Load LWM2M specifications (include default OMA classes for for Firmware profile)
+	public static void createClient(String endpoint, String serverURI) {
+		// Load LWM2M specs (include default OMA objects for Firmware profile)
 		List<ObjectModel> models = new ArrayList<>();
 		loadSpec(models, "/resources/oma-objects-spec.json");
 		loadSpec(models, "/resources/light-object-spec.json");
 		LwM2mModel model = new LwM2mModel(models);
 		ObjectsInitializer initializer = new ObjectsInitializer(model);
 
-		// register Objects by their ID
+		// Register mandatory objects (See Appendix E.1, E.2, E.4 of OMA TS)
+		initializer.setInstancesForObject(LwM2mId.SECURITY, noSec(serverURI, 123));
+		initializer.setInstancesForObject(LwM2mId.SERVER, new Server(123, 30, BindingMode.U, false));
+		// Note: Device is mandatory (including things like reboot), but this is
+		// not fully implemented by Leshan.
+		initializer.setInstancesForObject(LwM2mId.DEVICE, new Device("Group 19", "RPi", "12345", "U"));
+
+		// register other Objects by their ID
 		initializer.setClassForObject(LIGHT_PROFILE_ID, LightDevice.class);
 
 		// creates the Object Instances
-		List<LwM2mObjectEnabler> enablers = initializer.create(LIGHT_PROFILE_ID, FIRMWARE);
+		List<LwM2mObjectEnabler> enablers = initializer.create(LwM2mId.SECURITY, LwM2mId.SERVER, LwM2mId.DEVICE,
+				LIGHT_PROFILE_ID, LwM2mId.FIRMWARE);
 
 		// Create client
 		LeshanClientBuilder builder = new LeshanClientBuilder(endpoint);
