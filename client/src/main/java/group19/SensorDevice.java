891cc42766f1bf0caf29e6c200eb92aeee6218f6
alirezaghavamipour <alireza.ghavamipour@gmail.com>
2017-01-08 14:06:42 +0100

    sensor class added v1

---
+++ client/src/main/java/group19/SensorDevice.java
@@ -0,0 +1,93 @@
+package group19;
+
+import org.eclipse.leshan.client.resource.BaseInstanceEnabler;
+import org.eclipse.leshan.core.node.LwM2mResource;
+import org.eclipse.leshan.core.response.ExecuteResponse;
+import org.eclipse.leshan.core.response.ReadResponse;
+import org.eclipse.leshan.core.response.WriteResponse;
+
+public class SensorDevice extends BaseInstanceEnabler {
+	private String sensorId = "";
+	private String deviceType = "Sensor Device";
+	private SensorState sensorState = SensorState.FREE;
+	private String userId = "";
+	private int groupNo;
+	private float locationX;
+	private float locationY;
+	private String roomId = "";
+
+	@Override
+	public ReadResponse read(int resourceid) {
+		switch (resourceid) {
+		case 0:
+			return ReadResponse.success(resourceid, sensorId);
+		case 1:
+			return ReadResponse.success(resourceid, deviceType);
+		case 2:
+			return ReadResponse.success(resourceid, sensorState.name());
+		case 3:
+			return ReadResponse.success(resourceid, userId);
+		case 4:
+			return ReadResponse.success(resourceid, groupNo);
+		case 5:
+			return ReadResponse.success(resourceid, locationX);
+		case 6:
+			return ReadResponse.success(resourceid, locationY);
+		case 7:
+			return ReadResponse.success(resourceid, roomId);
+
+		default:
+			return super.read(resourceid);
+		}
+	}
+
+	@Override
+	public WriteResponse write(int resourceid, LwM2mResource value) {
+		switch (resourceid) {
+		case 0:
+			sensorId = (String) value.getValue();
+			return WriteResponse.success();
+		case 1:
+			deviceType = (String) value.getValue();
+			return WriteResponse.success();
+		case 2:
+			try {
+				sensorState = SensorState.valueOf((String) value.getValue());
+			} catch (IllegalArgumentException ex) {
+				return WriteResponse.badRequest("Invalid argument");
+			}
+			return WriteResponse.success();
+		case 3:
+			userId = (String) value.getValue();
+			return WriteResponse.success();
+		case 4:
+			groupNo = (int) value.getValue();
+			return WriteResponse.success();
+		case 5:
+			locationX = (float) value.getValue();
+			return WriteResponse.success();
+		case 6:
+			locationY = (float) value.getValue();
+			return WriteResponse.success();
+		case 7:
+			roomId = (String) value.getValue();
+			return WriteResponse.success();
+		default:
+			return super.write(resourceid, value);
+		}
+	}
+
+	@Override
+	public ExecuteResponse execute(int resourceid, String params) {
+		switch (resourceid) {
+		default:
+			return super.execute(resourceid, params);
+		}
+	}
+
+	// represents the state of the sensor device
+	enum SensorState {
+		USED, FREE
+	}
+
+}
