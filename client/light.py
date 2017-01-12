a07ad65b1d9ba4185f8b052fc18cd6812ec12c21
alirezaghavamipour <alireza.ghavamipour@gmail.com>
2017-01-12 13:22:32 +0100

    implement light.py

--- client/light.py
+++ client/light.py
@@ -2,13 +2,17 @@
 # Sets the light state based on commands from input.
 # See RPiLightDevice.java
 
+from sense_hat import SenseHat
 import sys
+
+sense = SenseHat()
 
 for line in sys.stdin:
     command, args = line.strip().split(" ", 1)
     if command == "color":
         r, g, b = map(int, args.split())
-        pass  # TODO
+        load_color = [(r, g, b)] * 64
+        sense.set_pixels(load_color)
     elif command == "lowlight":
         lowLight = args == "true"
-        pass  # TODO
+        sense.low_light = lowLight
